15b2a579d0215d0e79a448b5f96cdf795ffe99a3
augustin <augustin.charly@hotmail.fr>
2020-02-14 22:53:08 +0100

    mise à jour mineur

--- src/app/components/biscuit-list/biscuit-list.component.ts
+++ src/app/components/biscuit-list/biscuit-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Biscuit } from 'src/app/models/biscuit';
 import { BiscuitService } from 'src/app/services/biscuit.service';
-import { ActivatedRoute, Router, Event, NavigationEnd, NavigationError } from '@angular/router';
+import { ActivatedRoute, Router, Event, NavigationEnd, NavigationError, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-biscuit-list',
@@ -21,14 +21,16 @@ export class BiscuitListComponent implements OnInit {
   regExType = /^\/biscuits\/type\/.*/;
   regExName = /^\/biscuits\/search\/.*/;
   searchRegex: RegExp;
-  validCategorie: boolean;
+  valideCategorie: boolean;
 
   constructor(private biscuitService: BiscuitService, private activatedRoute: ActivatedRoute, private routeur: Router) {
     routeur.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+
+      }
       if (event instanceof NavigationEnd) {
         this.refreshBiscuits();
       }
-
       if (event instanceof NavigationError) {
         console.log(event.error);
       }
@@ -48,16 +50,20 @@ export class BiscuitListComponent implements OnInit {
   }
 
   refreshBiscuits() {
+    this.valideCategorie = true;
     this.categories = this.biscuitService.categories;
     this.url = this.routeur.url;
     this.beautyDisplay = this.biscuitService.getDisplay();
     if (this.regExType.test(this.url)) {
       this.type = this.activatedRoute.snapshot.paramMap.get('type');
-      this.validCategorie = (this.categories.indexOf(this.type) !== -1);
+      if (this.categories.indexOf(this.type) == -1) {
+        this.valideCategorie = false;
+      }
       return this.biscuitService.getBiscuits().subscribe((data: Biscuit[]) => {
         this.biscuits = data.filter(biscuit => biscuit.categorie === this.type);
         this.isLoading = false;
       });
+
     } else if (this.regExName.test(this.url)) {
       this.search = this.activatedRoute.snapshot.paramMap.get('querry');
       this.searchRegex = new RegExp(this.search.toUpperCase());
@@ -66,7 +72,6 @@ export class BiscuitListComponent implements OnInit {
         this.isLoading = false;
       });
     }
-
     return this.biscuitService.getBiscuits().subscribe((data: Biscuit[]) => {
       this.biscuits = data;
       this.isLoading = false;
