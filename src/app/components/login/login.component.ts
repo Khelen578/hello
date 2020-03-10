1203a566409af7a3c24139bbebee85eb1e0382ac
Augustin <Khelen578@live.fr>
2020-03-10 23:09:06 +0100

    auth commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, private routeur: Router) { }
 
   ngOnInit() {
-    this.loginUser = new User('kiichigo', 'abps2358');
+    this.loginUser = new User('kiichigo', '');
   }
 
   onLogin(): void {
