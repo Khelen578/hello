816f659b71650c8d38fdd47239a4a63e79e9ae4d
Augustin <Khelen578@live.fr>
2020-03-11 19:13:36 +0100

    update auth

--- src/app/components/biscuit/biscuit.component.ts
+++ src/app/components/biscuit/biscuit.component.ts
@@ -28,6 +28,13 @@ export class BiscuitComponent implements OnInit {
     this.isAuth = this.authService.getIsAuth();
   }
 
+  checkAuth(template: TemplateRef<any>, auth: TemplateRef<any>){
+    if (this.authService.getIsAuth()) {
+      this.modalRef = this.modalService.show(template);
+    } else {
+      this.modalRef = this.modalService.show(auth);
+    }
+  }
   onDeleteBiscuit(biscuit: Biscuit) {
     this.deleteEmitter.emit(this.biscuit);
     this.modalRef.hide();
@@ -40,10 +47,6 @@ export class BiscuitComponent implements OnInit {
 
   navigate() {
     this.routeur.navigate(['/biscuit-form-edit', this.biscuit.id]);
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
   }
 
 }
